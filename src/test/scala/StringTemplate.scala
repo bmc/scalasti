97cbdf1bda9af8d7bede434df8c4a97954a4d203
Brian M. Clapper <bmc@clapper.org>
2010-04-06 23:05:25 -0400

    Added some tests

---
+++ src/test/scala/StringTemplate.scala
@@ -0,0 +1,84 @@
+/*---------------------------------------------------------------------------*\
+  This software is released under a BSD license, adapted from
+  http://opensource.org/licenses/bsd-license.php
+
+  Copyright (c) 2010 Brian M. Clapper. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+  * Neither the names "clapper.org", "Scalasti", nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+\*---------------------------------------------------------------------------*/
+
+import org.scalatest.FunSuite
+import org.clapper.scalasti._
+
+/**
+ * Tests the grizzled.io functions.
+ */
+class StringTemplateTest extends ScalastiFunSuite
+{
+    test("render template #1")
+    {
+        val template = """This is a $test$ template: $many; separator=", "$"""
+
+        val data = List(
+            (Map("test" -> "test",
+                 "many" -> List("a", "b", "c")),
+             """This is a test template: a, b, c"""),
+
+            (Map("test" -> "foo",
+                 "many" -> List("moe", "larry", "curley")),
+             """This is a foo template: moe, larry, curley""")
+        )
+
+        for((attributes, expected) <- data)
+            expect(expected, "render template on: " + attributes)
+            {
+                val st = new StringTemplate(template)
+                st.setAttributes(attributes)
+                st.toString
+            }
+    }
+
+    test("ValueRenderer")
+    {
+        val template = """This is a $test$ template"""
+
+        class Value(val s: String)
+        class ValueRenderer extends AttributeRenderer[Value]
+        {
+            def toString(v: Value) = "<" + v.s + ">"
+        }
+
+        expect("This is a <foo> template", "ValueRenderer")
+        {
+            val st = new StringTemplate(template)
+            st.setAttribute("test", new Value("foo"))
+            st.registerRenderer(new ValueRenderer)
+            st.toString
+        }
+    }
+}
---
+++ src/test/scala/Suite.scala
@@ -0,0 +1,75 @@
+import scala.reflect.Manifest
+import org.scalatest.{FunSuite, Assertions}
+
+/**
+ * A version of ScalaTest's FunSuite that actually aborts the running test
+ * if an <tt>expect()</tt> or an <tt>intercept()</tt> fails.
+ */
+class ScalastiFunSuite extends FunSuite
+{
+    override def intercept[T <: AnyRef](f: => Any)
+                                       (implicit manifest: Manifest[T]): T =
+    {
+        try
+        {
+            super.intercept(f)(manifest)
+        }
+
+        catch
+        {
+            case ex: java.lang.Throwable =>
+                ex.printStackTrace()
+                System.exit(1)
+                throw ex // not reached, but keeps Scala happy
+        }
+    }
+
+    override def expect(expected: Any, message: Any)(actual: Any) 
+    {
+        try
+        {
+            super.expect(expected, message)(actual)
+        }
+        catch
+        {
+            case ex: java.lang.Throwable =>
+                ex.printStackTrace()
+                System.exit(1)
+        }
+    }
+
+    /**
+     * Sets the specified values in the system properties, runs the
+     * the specified code block, and restores the environment.
+     *
+     * @param code  code block to run
+     */
+    def withProperties(properties: Map[String, String])(code: => Any)
+    {
+        import scala.collection.mutable
+
+        val old = mutable.Map[String, String]()
+        for ((key, value) <- properties)
+        {
+            val oldValue = 
+                if (System.getProperty(key) == null)
+                    ""
+                else
+                    System.getProperty(key)
+            
+            old += key -> oldValue
+            System.setProperty(key, value)
+        }
+
+        try
+        {
+            code
+        }
+
+        finally
+        {
+            for ((key, value) <- old)
+                System.setProperty(key, value)
+        }
+    }
+}

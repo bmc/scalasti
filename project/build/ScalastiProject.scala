b262854e62977fa633aa0c82ac30f42390ebb30a
Brian M. Clapper <bmc@clapper.org>
2010-04-06 23:13:07 -0400

    Added publish-to information

--- project/build/ScalastiProject.scala
+++ project/build/ScalastiProject.scala
@@ -48,6 +48,18 @@ extends DefaultProject(info) with MarkdownPlugin with posterous.Publish
     val grizzled = "org.clapper" % "grizzled-scala" % "0.4.2"
 
     /* ---------------------------------------------------------------------- *\
+                                Publishing
+    \* ---------------------------------------------------------------------- */
+
+    // "publish" will prompt (via a Swing pop-up) for the username and
+    // password.
+    lazy val publishTo = Resolver.sftp("clapper.org Maven Repo",
+                                       "maven.clapper.org",
+                                       "/var/www/maven.clapper.org/html")
+
+    override def managedStyle = ManagedStyle.Maven
+
+    /* ---------------------------------------------------------------------- *\
                                    Tasks
     \* ---------------------------------------------------------------------- */
 
